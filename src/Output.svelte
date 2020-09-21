052d775d442e13dd8a994a99d07d91786f1f1f62
Alister Pineda <alister@alisterpineda.com>
2020-09-21 15:35:17 -0600

    Fix the value deltas to two decimal places

--- src/Output.svelte
+++ src/Output.svelte
@@ -21,17 +21,17 @@
 {#if position.deltaQuantity > 0}
 <p class="buy-result">Buy <b>{position.deltaQuantity}</b> units of <b>{position.symbol}</b></p>
 <p class="extra-info">{position.quantity} &rarr; {position.quantity + position.deltaQuantity}</p>
-<p class="extra-info">${position.getValue(false)} &rarr; ${position.getValue(true)}</p>
+<p class="extra-info">${position.getValue(false).toFixed(2)} &rarr; ${position.getValue(true).toFixed(2)}</p>
 <p class="extra-info">{($outputAccount.getAllocation(position, false)*100).toFixed(2)}% &rarr; {($outputAccount.getAllocation(position, true)*100).toFixed(2)}% (target: {($outputAccount.getTargetAllocation(position)*100).toFixed(2)}%)</p>
 {:else if position.deltaQuantity < 0}
 <p class="sell-result">Sell <b>{Math.abs(position.deltaQuantity)}</b> units of <b>{position.symbol}</b></p>
 <p class="extra-info">{position.quantity} &rarr; {position.quantity + position.deltaQuantity}</p>
-<p class="extra-info">${position.getValue(false)} &rarr; ${position.getValue(true)}</p>
+<p class="extra-info">${position.getValue(false).toFixed(2)} &rarr; ${position.getValue(true).toFixed(2)}</p>
 <p class="extra-info">{($outputAccount.getAllocation(position, false)*100).toFixed(2)}% &rarr; {($outputAccount.getAllocation(position, true)*100).toFixed(2)}% (target: {($outputAccount.getTargetAllocation(position)*100).toFixed(2)}%)</p>
 {:else}
 <p><b>{position.symbol}</b> stays the same</p>
 <p class="extra-info">{position.quantity} &rarr; {position.quantity + position.deltaQuantity}</p>
-<p class="extra-info">${position.getValue(false)} &rarr; ${position.getValue(true)}</p>
+<p class="extra-info">${position.getValue(false).toFixed(2)} &rarr; ${position.getValue(true).toFixed(2)}</p>
 <p class="extra-info">{($outputAccount.getAllocation(position, false)*100).toFixed(2)}% &rarr; {($outputAccount.getAllocation(position, true)*100).toFixed(2)}% (target: {($outputAccount.getTargetAllocation(position)*100).toFixed(2)}%)</p>
 {/if}
 {/each}
